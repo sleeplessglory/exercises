10fa2d4c93901e8e510819f8966ffbfce9d92b09
sleeplessglory <91571026+sleeplessglory@users.noreply.github.com>
2024-09-30 23:57:17 +0300

    Started a webpack course

---
+++ webpack/webpack.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    entry: './src/main.jsx' //entry point of the app (where it begins)
+    //which file should be used to create a dependency graph (which modules should be used first)
+
+}
